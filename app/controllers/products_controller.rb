b27c9059ae2079ee64750a1a5c9505c3db5a86a3
nozakifamima <saginogi@gmail.com>
2020-07-18 18:03:32 +0900

    debug1 commit

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,15 @@
 class ProductsController < ApplicationController
-  def index
+  def index  
+    @genres =Genre.all
+    if params[:genre_id]
+      @genre = Genre.find(params[:genre_id])
+      @products = @genre.product.all
+    else
+      @products = Product.all
+    end
   end
 
   def show
+    @product = Product.find(params[:product_id])
   end
 end
