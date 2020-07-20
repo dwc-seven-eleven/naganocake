8a89b295cdc482d90fa138257ef15b1072512036
nozakifamima <saginogi@gmail.com>
2020-07-20 12:02:35 +0900

    second commit

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -3,6 +3,13 @@ class CartsController < ApplicationController
   end
 
   def create
+    @cart = Cart.new
+    @cart.user_id = current_user.id
+    if @cart.save
+      redirect_to cart_path(params[:id])
+    else
+      redirect_to products_path
+    end
   end
 
   def update
--- config/routes.rb
+++ config/routes.rb
@@ -6,14 +6,14 @@ Rails.application.routes.draw do
   resources :products, only: [:index, :show]
   resources :carts, only: [:show, :create, :update, :destroy, :destroy_all]
   resources :orders, only: [:index, :new, :create, :show, :confirm, :complete]
-  resources :users, only: [:show, :leave, :edit, :update]
+  resources :users, only: [:show, :edit, :update]
+  get 'user/:id/leave' => 'users#leave', as: 'leave_user' 
   resources :shipping_addresses, only: [:index, :create, :destroy, :edit, :update]
   get 'admin_homes/top' => 'admin_homes#top'
   resources :admin_products, only: [:index, :new, :create, :show, :edit, :update]
   resources :admin_genres, only: [:index, :create, :edit, :update, :destroy]
   resources :admin_users,only: [:index, :show, :update, :edit]
   resources :admin_orders, only: [:index, :show, :update]
-
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
