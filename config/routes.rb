0f5511ce68acbc1d1e660481eb01b15c9a72ca7f
shiho-h <sisopiku@gmail.com>
2020-07-23 16:04:48 +0900

    adminslogin

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :admins
+ devise_for :admins, controllers: { sessions: 'admins/sessions' }
   devise_for :users
   root to: 'homes#top'
   get 'homes/about' => 'homes#about'
