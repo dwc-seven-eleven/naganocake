a5f26da8615b3201fcf273e5ca55180f589b55ca
vagrant <vagrant@localhost.localdomain>
2020-07-26 14:57:35 +0000

    エラー修正

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,12 @@ module Naganocake
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    #railsが表示の際に扱うタイムゾーン
+    config.time_zone = 'Tokyo'
+
+    #rails(Activerecord)がDBへのRead.Writeを行う際のタイムゾーン
+    config.active_record.default_timezone = :local
+
   end
 end
