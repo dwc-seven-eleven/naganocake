b31b60e653c42f71205f0e20938fe70b7efa5ab2
vagrant <vagrant@localhost.localdomain>
2020-07-26 16:26:01 +0000

    detail

--- app/controllers/admin_orders_controller.rb
+++ app/controllers/admin_orders_controller.rb
@@ -13,16 +13,17 @@ class AdminOrdersController < ApplicationController
 
 # 該当顧客の注文一覧
   def detail
-    @orders = Order.where(user_id: user.id)
-    @order_products = Order.where(user_id: user.id)
+    @order_products = OrderProduct.page(params[:page]).reverse_order
+    @oder = Order.find_by(params[:id])
   end
+
 
 # 当日の注文履歴一覧
   def today
   end
 
 private
-  def cart_params
+  def order_params
     params.require(:order).permit(:user_id, :total_price, :pay, :status, :shipping_name, :shipping_postcode, :shipping_adress, :postage, :created_at)
   end
 
