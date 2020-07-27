81bdbc8c53bbee9cf661b7673c65ecebfb733339
vagrant <vagrant@localhost.localdomain>
2020-07-27 05:01:18 +0000

    toppage

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,7 +1,13 @@
 class HomesController < ApplicationController
   def top
-    @users = User.all
-
+  	@genres =Genre.all
+    if params[:genre_id]
+      @genre = Genre.find(params[:genre_id])
+      @products = @genre.product.page(params[:page]).reverse_order
+    else
+      @products = Product.page(params[:page]).reverse_order
+    end
+    @random = Product.order( "RANDOM()" ).limit(4)
   end
 
   def about
